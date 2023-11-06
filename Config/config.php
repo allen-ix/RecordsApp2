10edfc3d7896e4ff87f058f1bc1e303796c489fa
John Allen Dugelio <dugeliojohnallen@gmail.com>
2023-11-06 23:57:03 +0700

    a

---
+++ Config/config.php
@@ -0,0 +1,7 @@
+<?php 
+    define('ROOT_URL', 'http://localhost/recordsapp/');
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS','allen');
+    define('DB_NAME','recordsapp_db');
+?>
